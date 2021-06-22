00f92e02d5876771056458a32f21e9ba01523c0f
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-06-22 17:02:20 +0800

    most functionalities added

--- main.js
+++ main.js
@@ -1,1 +1,115 @@
-console.log('Hello world');
+const prevOperandDisplay = document.querySelector('#prevOperand');
+const currOperandDisplay = document.querySelector('#currOperand');
+const numBtns = document.querySelectorAll('.num');
+const operationBtns = document.querySelectorAll('.operation');
+const deleteBtns = document.querySelectorAll('.delete');
+
+let prevOperand = '';
+let operation = '';
+let currOperand = '';
+
+function appendNum(num) {
+	if(num === '.') {
+		if(currOperand.includes('.')) return;
+
+		if(currOperand === '') {
+			currOperand = '0';
+		}
+
+		currOperand += '.';
+	}else {
+		currOperand = (+(currOperand + num)).toString();
+	}
+}
+
+function setOperation(str) {
+	if(currOperand === '') return;
+
+	if(str === '=') {
+		compute();
+
+		setOperation(str);
+	}else if(currOperand !== '' && prevOperand !== '') {
+		compute();
+		setOperation(str);
+	}else if(prevOperand === '') {
+		prevOperand = currOperand;
+		currOperand = '';
+		operation = str;
+	}
+}
+
+function compute() {
+	if(+currOperand === 0) return;
+
+	switch(operation) {
+		case '+':
+			currOperand = +prevOperand + +currOperand;
+			break;
+
+		case '-':
+			currOperand = +prevOperand - +currOperand;
+			break;
+
+		case '×':
+			currOperand = +prevOperand * +currOperand;
+			break;
+
+		case '÷':
+			currOperand = +((+prevOperand / +currOperand).toFixed(2)).toString();
+			break;
+
+		default:
+			break;
+	}
+
+	prevOperand = '';
+	operation = '';
+}
+
+function deleteNum(deleteType) {
+	switch(deleteType) {
+		case 'DEL':
+			currOperand = currOperand.slice(0, -1);
+			break;
+
+		case 'CE':
+			prevOperand = '';
+			operation = '';
+			currOperand = '';
+			break;
+
+		case 'C':
+			currOperand = '';
+			break;
+
+		default:
+			break;
+	}
+}
+
+function updateDisplay() {
+	prevOperandDisplay.textContent = `${prevOperand} ${operation}`;
+	currOperandDisplay.textContent = currOperand;
+}
+
+for(const numBtn of numBtns) {
+	numBtn.addEventListener('click', (e) => {
+		appendNum(e.target.textContent);
+		updateDisplay();
+	});
+}
+
+for(const operationBtn of operationBtns) {
+	operationBtn.addEventListener('click', (e) => {
+		setOperation(e.target.textContent);
+		updateDisplay();
+	});
+}
+
+for(const deleteBtn of deleteBtns) {
+	deleteBtn.addEventListener('click', (e) => {
+		deleteNum(e.target.textContent);
+		updateDisplay();
+	});
+}
