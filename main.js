438fc6412ccc852ce19722629dd71180a8e81b3a
Clikuki <73450123+clikuki@users.noreply.github.com>
2021-06-23 10:58:16 +0800

    comments and some other changes

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ const operationBtns = document.querySelectorAll('.operation');
 const deleteBtns = document.querySelectorAll('.delete');
 
 let prevOperand = '';
-let operation = '';
+let currOperation = '';
 let currOperand = '';
 
+// append a number to the current operand
 function appendNum(num) {
 	if(num === '.') {
 		if(currOperand.includes('.')) return;
@@ -22,27 +23,27 @@ function appendNum(num) {
 	}
 }
 
-function setOperation(str) {
+// set the operation
+function setOperation(operation) {
+	// don't set operation if current operand is empty
 	if(currOperand === '') return;
 
-	if(str === '=') {
+	if(currOperand !== ''
+	&& prevOperand !== '') {
+		// if current and previous operand arent empty, then compute and set operation
 		compute();
-
-		setOperation(str);
-	}else if(currOperand !== '' && prevOperand !== '') {
-		compute();
-		setOperation(str);
-	}else if(prevOperand === '') {
-		prevOperand = currOperand;
-		currOperand = '';
-		operation = str;
 	}
+
+	if(operation === '=') return;
+
+	// else, move the current operand to previous operand and set operation
+	prevOperand = currOperand;
+	currOperand = '';
+	currOperation = operation;
 }
 
 function compute() {
-	if(+currOperand === 0) return;
-
-	switch(operation) {
+	switch(currOperation) {
 		case '+':
 			currOperand = +prevOperand + +currOperand;
 			break;
@@ -56,6 +57,7 @@ function compute() {
 			break;
 
 		case '÷':
+			if(+currOperand === 0) return;
 			currOperand = +((+prevOperand / +currOperand).toFixed(2)).toString();
 			break;
 
@@ -64,21 +66,25 @@ function compute() {
 	}
 
 	prevOperand = '';
-	operation = '';
+	currOperation = '';
 }
 
+// does all the delete functions
 function deleteNum(deleteType) {
 	switch(deleteType) {
+		// DEL removes the last number inputed from the current operand
 		case 'DEL':
 			currOperand = currOperand.slice(0, -1);
 			break;
 
+		// CE resets all of the operands and the operation
 		case 'CE':
 			prevOperand = '';
-			operation = '';
+			currOperation = '';
 			currOperand = '';
 			break;
 
+		// C clears the current operand
 		case 'C':
 			currOperand = '';
 			break;
@@ -89,27 +95,31 @@ function deleteNum(deleteType) {
 }
 
 function updateDisplay() {
-	prevOperandDisplay.textContent = `${prevOperand} ${operation}`;
+	prevOperandDisplay.textContent = `${prevOperand} ${currOperation}`;
 	currOperandDisplay.textContent = currOperand;
 }
 
-for(const numBtn of numBtns) {
-	numBtn.addEventListener('click', (e) => {
-		appendNum(e.target.textContent);
-		updateDisplay();
-	});
+function setEventListeners() {
+	for(const btn of numBtns) {
+		btn.addEventListener('click', (e) => {
+			appendNum(e.target.textContent);
+			updateDisplay();
+		});
+	}
+
+	for(const btn of operationBtns) {
+		btn.addEventListener('click', (e) => {
+			setOperation(e.target.textContent);
+			updateDisplay();
+		});
+	}
+
+	for(const btn of deleteBtns) {
+		btn.addEventListener('click', (e) => {
+			deleteNum(e.target.textContent);
+			updateDisplay();
+		});
+	}
 }
 
-for(const operationBtn of operationBtns) {
-	operationBtn.addEventListener('click', (e) => {
-		setOperation(e.target.textContent);
-		updateDisplay();
-	});
-}
-
-for(const deleteBtn of deleteBtns) {
-	deleteBtn.addEventListener('click', (e) => {
-		deleteNum(e.target.textContent);
-		updateDisplay();
-	});
-}
+setEventListeners();
